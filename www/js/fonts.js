0da2afb7cfa4d58a4882a8f2e3c4a432b153a8a8
lindamood <wesley.lindamood@gmail.com>
2015-01-08 14:50:26 -0500

    adding knockout to the mix

--- www/js/fonts.js
+++ www/js/fonts.js
@@ -1,12 +1,14 @@
 var urls = [
     'http://s.npr.org/templates/css/fonts/GothamSSm.css',
-    'http://s.npr.org/templates/css/fonts/Gotham.css'
+    'http://s.npr.org/templates/css/fonts/Gotham.css',
+    'http://s.npr.org/templates/css/fonts/Knockout.css'
 ];
 
 if (window.location.protocol == "https:") {
     urls = [
         'https://secure.npr.org/templates/css/fonts/GothamSSm.css',
-        'https://secure.npr.org/templates/css/fonts/Gotham.css'
+        'https://secure.npr.org/templates/css/fonts/Gotham.css',
+        'http://s.npr.org/templates/css/fonts/Knockout.css'
     ];
 }
 
@@ -14,7 +16,8 @@ WebFont.load({
      custom: {
          families: [
              'Gotham SSm:n4,n7',
-             'Gotham:n4,n7'
+             'Gotham:n4,n7',
+             'Knockout 31 4r:n4'
          ],
          urls: urls
      },

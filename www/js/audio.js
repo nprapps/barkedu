c207f79218beee878ab0302bde6ac2777c243152
Tyler Fisher <tyler@tylerjfisher.com>
2015-02-02 10:53:01 -0500

    add subtle fades if on desktop

--- www/js/audio.js
+++ www/js/audio.js
@@ -154,9 +154,27 @@ var AUDIO = (function() {
     }
 
     var setAmbientMedia = function(url) {
-        $ambientPlayer.jPlayer('setMedia', {
-            mp3: url
-        }).jPlayer('play');
+        if (!isTouch) {
+            if (!$ambientPlayer.data().jPlayer.status.paused) {
+                $ambientPlayer.jPlayerFade().to(250, 1, 0, function() {
+                    $ambientPlayer.jPlayer('setMedia', {
+                        mp3: url
+                    }).jPlayer('play');
+
+                    $ambientPlayer.jPlayerFade().to(250, 0, 1);
+
+                });
+            } else {
+                $ambientPlayer.jPlayer('setMedia', {
+                    mp3: url
+                }).jPlayer('play');
+                $ambientPlayer.jPlayerFade().to(250, 0, 1);
+            }
+        } else {
+            $ambientPlayer.jPlayer('setMedia', {
+                mp3: url
+            }).jPlayer('play');
+        }
     }
 
     var fakeAmbientPlayer = function() {
